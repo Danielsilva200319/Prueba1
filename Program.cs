3c8a83ca657f8354fba48188b189bce8721aef60
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-12-04 13:15:50 -0500

    new

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Escriba un número entero: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"F{num} = ");
     }
 }
